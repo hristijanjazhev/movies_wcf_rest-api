a3806573a036020c40be884ed40d56e43d453215
hristijan_j <45484589+hristijan11@users.noreply.github.com>
2022-03-10 01:27:22 -0800

    Updating the code

--- FilterMovies/Program.cs
+++ FilterMovies/Program.cs
@@ -9,9 +9,6 @@ namespace FilterMovies
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
